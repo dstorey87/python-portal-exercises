01c4b4c60a4098761967a9e48cda82bcbc82fc79
dstorey87 <104389936+dstorey87@users.noreply.github.com>
2025-08-31 00:30:29 +0100

    feat: Add TypeScript config, ESLint config, and comprehensive gitignore

---
+++ .eslintrc.js
@@ -0,0 +1,36 @@
+module.exports = {
+  parser: '@typescript-eslint/parser',
+  extends: [
+    '@typescript-eslint/recommended',
+    'eslint:recommended'
+  ],
+  plugins: [
+    '@typescript-eslint'
+  ],
+  parserOptions: {
+    ecmaVersion: 2022,
+    sourceType: 'module',
+    project: './tsconfig.json'
+  },
+  env: {
+    node: true,
+    es6: true
+  },
+  rules: {
+    '@typescript-eslint/no-unused-vars': 'error',
+    '@typescript-eslint/no-explicit-any': 'warn',
+    '@typescript-eslint/explicit-function-return-type': 'off',
+    '@typescript-eslint/explicit-module-boundary-types': 'off',
+    '@typescript-eslint/no-non-null-assertion': 'warn',
+    'no-console': 'off', // Allow console.log for server logging
+    'no-debugger': 'error',
+    'prefer-const': 'error',
+    'no-var': 'error'
+  },
+  ignorePatterns: [
+    'dist/',
+    'node_modules/',
+    'content/',
+    '*.js'
+  ]
+};
